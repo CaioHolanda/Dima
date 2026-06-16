1f7ef668d7460f1be7363fd91e2cb0d1e5777190
CAIO HOLANDA <caiojholanda@gmail.com>
2026-06-16 22:18:52 +0100

    WIP: Frontend Orders Initial

---
+++ Dima.Web/Handlers/VoucherHandler.cs
@@ -0,0 +1,17 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Order;
+using Dima.Core.Responses;
+using Microsoft.Extensions.Http;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers
+{
+    public class VoucherHandler(IHttpClientFactory httpClientFactory) : IVoucherHandler
+    {
+        private readonly HttpClient _client=httpClientFactory.CreateClient(Configuration.HttpClientName);
+        public async Task<Response<Voucher?>> GetByNumberAsync(GetVoucherByNumberRequest request)
+        =>await _client.GetFromJsonAsync<Response<Voucher?>>($"v1/voucher/{request.Number}")
+          ?? new Response<Voucher?>(null, 400, "[E064] Nao foi possivel obter o voucher");
+    }
+}
--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -27,6 +27,9 @@ builder.Services
 
 builder.Services.AddTransient<IAccountHandler,      AccountHandler>();
 builder.Services.AddTransient<ITransactionHandler,  TransactionHandler>();
+builder.Services.AddTransient<IOrderHandler,        OrderHandler>();
+builder.Services.AddTransient<IProductHandler,      ProductHandler>();
+builder.Services.AddTransient<IVoucherHandler,      VoucherHandler>();
 builder.Services.AddTransient<ICategoryHandler,     CategoryHandler>();
 builder.Services.AddTransient<IReportHandler,       ReportHandler>();
 
