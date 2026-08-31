a6b84ac7352496ffb3b98ca1f8be626785fbe7e4
Caio Holanda <caiojholanda@gmail.com>
2026-08-31 16:53:45 +0100

    Adicionando projeto de testes-R00

---
+++ Dima.Tests/Orders/Fakes/FakePaymentHandler.cs
@@ -0,0 +1,28 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Payment;
+using Dima.Core.Responses;
+
+namespace Dima.Tests.Orders.Fakes;
+
+public class FakePaymentHandler : IPaymentHandler
+{
+    public bool RefundWasCalled { get; private set; }
+
+    public Task<Response<string?>> CreateSessionAsync(
+        CreatePaymentSessionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<string?>> RefundAsync(
+        string externalReference)
+    {
+        RefundWasCalled = true;
+
+        return Task.FromResult(
+            new Response<string?>(
+                "re_test_refund",
+                200,
+                "Refund fake criado"));
+    }
+}
